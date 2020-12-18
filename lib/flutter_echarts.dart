9f02bd512407fd9a9d1dac8bf34a72056ce7c1de
mohanhari <mohan@francium.tech>
2020-12-18 18:37:23 +0530

    upgraded webview and trying to fix ios error

--- lib/flutter_echarts.dart
+++ lib/flutter_echarts.dart
@@ -7,6 +7,7 @@ import 'dart:io' show Platform;
 import 'package:flutter/widgets.dart';
 import 'package:flutter/gestures.dart';
 import 'package:flutter/foundation.dart';
+import 'package:webview_flutter/platform_interface.dart';
 
 import 'package:webview_flutter/webview_flutter.dart';
 
@@ -14,7 +15,8 @@ import 'echarts_script.dart' show echartsScript;
 
 /// <!DOCTYPE html><html><head><meta charset="utf-8"><meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0, user-scalable=0, target-densitydpi=device-dpi" /><style type="text/css">body,html,#chart{height: 100%;width: 100%;margin: 0px;}div {-webkit-tap-highlight-color:rgba(255,255,255,0);}</style></head><body><div id="chart" /></body></html>
 /// 'data:text/html;base64,' + base64Encode(const Utf8Encoder().convert( /* STRING ABOVE */ ))
-const htmlBase64 = 'data:text/html;base64,PCFET0NUWVBFIGh0bWw+PGh0bWw+PGhlYWQ+PG1ldGEgY2hhcnNldD0idXRmLTgiPjxtZXRhIG5hbWU9InZpZXdwb3J0IiBjb250ZW50PSJ3aWR0aD1kZXZpY2Utd2lkdGgsIGluaXRpYWwtc2NhbGU9MS4wLCBtYXhpbXVtLXNjYWxlPTEuMCwgbWluaW11bS1zY2FsZT0xLjAsIHVzZXItc2NhbGFibGU9MCwgdGFyZ2V0LWRlbnNpdHlkcGk9ZGV2aWNlLWRwaSIgLz48c3R5bGUgdHlwZT0idGV4dC9jc3MiPmJvZHksaHRtbCwjY2hhcnR7aGVpZ2h0OiAxMDAlO3dpZHRoOiAxMDAlO21hcmdpbjogMHB4O31kaXYgey13ZWJraXQtdGFwLWhpZ2hsaWdodC1jb2xvcjpyZ2JhKDI1NSwyNTUsMjU1LDApO308L3N0eWxlPjwvaGVhZD48Ym9keT48ZGl2IGlkPSJjaGFydCIgLz48L2JvZHk+PC9odG1sPg==';
+const htmlBase64 =
+    'data:text/html;base64,PCFET0NUWVBFIGh0bWw+PGh0bWw+PGhlYWQ+PG1ldGEgY2hhcnNldD0idXRmLTgiPjxtZXRhIG5hbWU9InZpZXdwb3J0IiBjb250ZW50PSJ3aWR0aD1kZXZpY2Utd2lkdGgsIGluaXRpYWwtc2NhbGU9MS4wLCBtYXhpbXVtLXNjYWxlPTEuMCwgbWluaW11bS1zY2FsZT0xLjAsIHVzZXItc2NhbGFibGU9MCwgdGFyZ2V0LWRlbnNpdHlkcGk9ZGV2aWNlLWRwaSIgLz48c3R5bGUgdHlwZT0idGV4dC9jc3MiPmJvZHksaHRtbCwjY2hhcnR7aGVpZ2h0OiAxMDAlO3dpZHRoOiAxMDAlO21hcmdpbjogMHB4O31kaXYgey13ZWJraXQtdGFwLWhpZ2hsaWdodC1jb2xvcjpyZ2JhKDI1NSwyNTUsMjU1LDApO308L3N0eWxlPjwvaGVhZD48Ym9keT48ZGl2IGlkPSJjaGFydCIgLz48L2JvZHk+PC9odG1sPg==';
 
 class Echarts extends StatefulWidget {
   Echarts({
@@ -69,11 +71,13 @@ class _EchartsState extends State<Echarts> {
 
   void init() async {
     final extensionsStr = this.widget.extensions.length > 0
-    ? this.widget.extensions.reduce(
-        (value, element) => (value ?? '') + '\n' + (element ?? '')
-      )
-    : '';
-    final themeStr = this.widget.theme != null ? '\'${this.widget.theme}\'' : 'null';
+        ? this
+            .widget
+            .extensions
+            .reduce((value, element) => (value ?? '') + '\n' + (element ?? ''))
+        : '';
+    final themeStr =
+        this.widget.theme != null ? '\'${this.widget.theme}\'' : 'null';
     await _controller?.evaluateJavascript('''
       $echartsScript
       $extensionsStr
@@ -88,7 +92,8 @@ class _EchartsState extends State<Echarts> {
 
   Set<Factory<OneSequenceGestureRecognizer>> getGestureRecognizers() {
     Set<Factory<OneSequenceGestureRecognizer>> set = Set();
-    if(this.widget.captureAllGestures || this.widget.captureHorizontalGestures) {
+    if (this.widget.captureAllGestures ||
+        this.widget.captureHorizontalGestures) {
       set.add(Factory<HorizontalDragGestureRecognizer>(() {
         return HorizontalDragGestureRecognizer()
           ..onStart = (DragStartDetails details) {}
@@ -98,7 +103,7 @@ class _EchartsState extends State<Echarts> {
           ..onEnd = (DragEndDetails details) {};
       }));
     }
-    if(this.widget.captureAllGestures || this.widget.captureVerticalGestures) {
+    if (this.widget.captureAllGestures || this.widget.captureVerticalGestures) {
       set.add(Factory<VerticalDragGestureRecognizer>(() {
         return VerticalDragGestureRecognizer()
           ..onStart = (DragStartDetails details) {}
@@ -143,32 +148,37 @@ class _EchartsState extends State<Echarts> {
   Widget build(BuildContext context) {
     // --- FIX_BLINK ---
     return Opacity(
-      opacity: _opacity,
-    // --- FIX_BLINK ---
-      child: WebView(
-        initialUrl: htmlBase64,
-        javascriptMode: JavascriptMode.unrestricted,
-        onWebViewCreated: (WebViewController webViewController) {
-          _controller = webViewController;
-        },
-        onPageFinished: (String url) {
-          // --- FIX_BLINK ---
-          if (Platform.isAndroid) {
-            setState(() { _opacity = 1.0; });
-          }
-          // --- FIX_BLINK ---
-          init();
-        },
-        javascriptChannels: <JavascriptChannel>[
-          JavascriptChannel(
-            name: 'Messager',
-            onMessageReceived: (JavascriptMessage javascriptMessage) {
-              widget?.onMessage(javascriptMessage.message);
-            }
-          ),
-        ].toSet(),
-        gestureRecognizers: getGestureRecognizers()
-      )
-    );
+        opacity: _opacity,
+        // --- FIX_BLINK ---
+        child: WebView(
+            initialUrl: htmlBase64,
+            javascriptMode: JavascriptMode.unrestricted,
+            onWebViewCreated: (WebViewController webViewController) {
+              _controller = webViewController;
+            },
+            onPageFinished: (String url) {
+              // --- FIX_BLINK ---
+              if (Platform.isAndroid) {
+                setState(() {
+                  _opacity = 1.0;
+                });
+              }
+              // --- FIX_BLINK ---
+              init();
+            },
+            onWebResourceError: (error) {
+              if (error.errorType ==
+                  WebResourceErrorType.webContentProcessTerminated) {
+                _controller.reload();
+              }
+            },
+            javascriptChannels: <JavascriptChannel>[
+              JavascriptChannel(
+                  name: 'Messager',
+                  onMessageReceived: (JavascriptMessage javascriptMessage) {
+                    widget?.onMessage(javascriptMessage.message);
+                  }),
+            ].toSet(),
+            gestureRecognizers: getGestureRecognizers()));
   }
 }
